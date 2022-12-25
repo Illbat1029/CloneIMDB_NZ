6ae2f4333c66e9e3342b4d6e19e885521e3dd0f1
illbat1029 <illbat1029@gmail.com>
2022-12-25 14:48:33 +0100

    testcommit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from DB_connector import *
 
 sql = """INSERT INTO user (uname, pass, email) VALUES (%s, %s, %s)"""
 data = [
-    ("MaksSSS", "654321", "maks@gmail.com"),
-    ("NikitaSSS", "123456", "nikita@gmail.com")
+    ("soso", "654321", "maks@gmail.com"),
+    ("Nsisi", "123456", "nikita@gmail.com")
 ]
 sqlGet = """SELECT * FROM user"""
 executemanyQuery(createConnection(),sql,data)
