5988e1ca31204e073b07bb52b7125136b4bb5df2
illbat1029 <illbat1029@gmail.com>
2023-01-09 18:15:02 +0100

    09.01.2023

--- LogicApplication/methodsAdminAndModerator.py
+++ LogicApplication/methodsAdminAndModerator.py
@@ -2,7 +2,8 @@ from LogicApplication.DB_connector import *
 import mysql.connector
 from LogicApplication.getDataFromIMDB import *
 class ReportReview:
-    def __init__(self, review, type):
+    def __init__(self, id, review, type):
+        self.reviw_id = id
         self.review = review
         self.type = type
 def addFilmsAdmin (idFilmStart, idFilmEnd=0):
@@ -27,7 +28,7 @@ def getAllReportReviews():
     con = createConnection()
     cur = con.cursor()
     sqlGetAllReportReviews = """
-    SELECT review, report_types.type_name FROM admin_review_validation
+    SELECT id_review, review, report_types.type_name FROM admin_review_validation
     JOIN films_review
     ON admin_review_validation.id_review = films_review.id
     JOIN report_types
@@ -38,7 +39,7 @@ def getAllReportReviews():
     retData = []
     for row in allReportReviews:
         buff = list(row)
-        rep = ReportReview(buff[0], buff[1])
+        rep = ReportReview(buff[0], buff[1], buff[2])
         retData.append(rep)
     return retData
 def deleteReviewAfterValidation(reviewId):
