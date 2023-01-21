638b762dfc77d2dc986eb61b68cfd76b9a4fd15d
illbat1029 <illbat1029@gmail.com>
2023-01-21 23:34:26 +0100

    ChangeV2

--- LogicApplication/methodsAdminAndModerator.py
+++ LogicApplication/methodsAdminAndModerator.py
@@ -3,10 +3,11 @@ import mysql.connector
 from LogicApplication.getDataFromIMDB import *
 from datetime import datetime, timedelta
 class ReportReview:
-    def __init__(self, id, review, type):
+    def __init__(self, id, review, type, username):
         self.reviw_id = id
         self.review = review
         self.type = type
+        self.username = username
 def addFilmsAdmin (idFilmStart, idFilmEnd=0):
     if (idFilmEnd == 0):
         idFilmEnd = idFilmStart+1
@@ -32,18 +33,20 @@ def getAllReportReviews():
     con = createConnection()
     cur = con.cursor()
     sqlGetAllReportReviews = """
-    SELECT id_review, review, report_types.type_name FROM admin_review_validation
+    SELECT id_review, review, report_types.type_name, u.username FROM admin_review_validation
     JOIN films_review
     ON admin_review_validation.id_review = films_review.id
     JOIN report_types
     ON admin_review_validation.id_report_type = report_types.id
+    JOIN user u 
+    ON u.id = films_review.id_user
     """
     cur.execute(sqlGetAllReportReviews)
     allReportReviews = cur.fetchall()
     retData = []
     for row in allReportReviews:
         buff = list(row)
-        rep = ReportReview(buff[0], buff[1], buff[2])
+        rep = ReportReview(buff[0], buff[1], buff[2], buff[3])
         retData.append(rep)
     print("Get all review by ADMIN = ", datetime.now() - stime)
     return retData
